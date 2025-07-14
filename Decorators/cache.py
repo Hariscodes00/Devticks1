fa51c53ade3ea218a6de540dd8d398caafeb9836
haris <hariscodes00@gmail.com>
2025-07-14 18:08:21 +0500

    decorators

---
+++ Decorators/cache.py
@@ -0,0 +1,21 @@
+# Implement a decorator that caches the return values of a function,
+# so that when it's called with the same arguments, the cached value is returned instead of re-executing the function.
+import time
+def cache(func):
+    cache_value = {}
+    print(cache_value)
+    def wrapper(*args):
+        if args in cache_value:
+            return cache_value[args]
+        result = func(*args)
+        cache_value[args] = result
+        return result
+    return wrapper
+@cache
+def long_time(a, b):
+    time.sleep(5)
+    return a+ b
+
+print(long_time(5,5))
+
+
